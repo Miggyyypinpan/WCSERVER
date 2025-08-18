9821c3dd4b44643148a486afa4839e6d7af61979
miggyyypinpan <miggypin1218@gmail.com>
2025-08-18 12:29:31 +0800

    hello

--- express.js
+++ express.js
@@ -1,12 +1,54 @@
 import express from 'express';
+import multer from 'multer';
+
+//File Path Stuff
 import path from 'path';
-import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const __dirname = import.meta.dirname;
-const app = express();
+var app = express();
 
-//Middleware to parse JSON and URL-encoded data
-const urlEncoderParser = bodyParser.urlencoded({extended: false});
+// Storage object, tells multer where to put the file and what name to give it
+var storage = multer.diskStorage({
+    // You can create a callback function to pass data back to the parent after a function if needed
+    // In this case we pass the data back up to multer via the callback()
+    destination: (req, file, callback ) => {
+        callback(null, 'uploads/');
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);
+    }
+});
+
+// Changed .single() (Expect only a single field), to .fields()
+// Where you need to specify input is the upload. In this case the "name" of the <input> in your html
+var upload = multer({storage: storage}).fields([{ name: 'file', maxCount: 1}]);;
+
+// Open form on index (/)
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, '/uploadForm.html'));
+    });
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        // Check if successful
+        if (err) return res.status(400).send('Error uploading file');
+        
+        const uploadedFile = req.files['file'][0];
+        var response = {
+            adminId: req.body.adminId,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            department: req.body.department,
+            uploadedFile: uploadedFile.path
+        }
+
+        console.log("Response is: ", response);
+        res.end(`Received Data: ${JSON.stringify(response)}`)
+    })
+});
 
 
 app.use(express.static('public'));
@@ -62,21 +104,28 @@ app.get('/getStudent', (req,res) =>{
 });
 
 // Handles the Admin Form submission
-app.post('/postAdmin', urlEncoderParser, (req,res) =>{
-    var response = {
-        adminId: req.body.adminId,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        department: req.body.department
-    }
+app.post('/postAdmin', (req,res) =>{
+    upload(req, res, (err) =>{
+        if (err) return res.status(400).send('Error uploading file');
+        
+        const uploadedFile = req.files['file'][0];
+        var response = {
+            adminId: req.body.adminId,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            department: req.body.department,
+            uploadedFile: uploadedFile.path
+        }
 
-    console.log("Response is: ", response);
-    res.end(JSON.stringify(response));
+        console.log("Response is: ", response);
+        res.end(`Received Data: ${JSON.stringify(response)}`)
+    })
 });
 
-// Start the server
-const server = app.listen(5000, () =>{
+// Listen to port
+var server = app.listen(5001, () =>{
     const host = server.address().address;
     const port = server.address().port;
-    console.log(`Server running at http://${host}:${port}`);
-})
+
+    console.log(`Server running at http://%s:%s/`, host, port);
+});
